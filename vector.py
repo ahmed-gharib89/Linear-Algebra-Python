f83d6a6ca765b6b82a01ec8b09c8f3b857079179
Ahmed_Gharib <A.Gharib89@yahoo.com>
2020-07-05 15:31:42 +0200

    Editing code to asure that all the numbers are decimal

--- vector.py
+++ vector.py
@@ -1,11 +1,18 @@
 import math
+from decimal import Decimal, getcontext
+
+getcontext().prec = 30
+
 class Vector(object):
+
+    CANNOT_NORMALIZE_ZERO_VECTOR_MSG = 'Cannot normalize the zero vector'
+
     def __init__(self, coordinates):
         try:
             if not coordinates:
                 raise ValueError
-            self.coordinates = tuple(coordinates)
-            self.dimension = len(coordinates)
+            self.coordinates = tuple([Decimal(x) for x in coordinates])
+            self.dimension = len(self.coordinates)
 
         except ValueError:
             raise ValueError('The coordinates must be nonempty')
@@ -23,25 +30,25 @@ class Vector(object):
         if self.dimension != v.dimension:
             raise ValueError('The 2 vectors must have the same dimensions')
         else:
-            return Vector([self.coordinates[i] + v.coordinates[i] for i in range(len(self.coordinates))])
+            return Vector([x + y for x, y in zip(self.coordinates, v.coordinates)])
 
     def __sub__(self, v):
         if self.dimension != v.dimension:
             raise ValueError('The 2 vectors must have the same dimensions')
         else:
-            return Vector([self.coordinates[i] - v.coordinates[i] for i in range(len(self.coordinates))])
+            return Vector([x - y for x, y in zip(self.coordinates, v.coordinates)])
 
-    def times_scaler(self, n):
-        return Vector([x * n for x in self.coordinates])
+    def times_scaler(self, c):
+        return Vector([x * Decimal(c) for x in self.coordinates])
 
     def magnitude(self):
-        return math.sqrt(sum([x ** 2 for x in self.coordinates]))
+        return Decimal(math.sqrt(sum([x ** 2 for x in self.coordinates])))
 
     def normalized(self):
         try:
-            return self.times_scaler(1 / self.magnitude())
+            return self.times_scaler(Decimal('1.0') / self.magnitude())
         except ZeroDivisionError:
-            raise Exception('Cannot normalize the zero vector')
+            raise Exception(self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG)
 
     def dot(self, v):
         return sum([x * y for x,y in zip(self.coordinates, v.coordinates)])
@@ -63,6 +70,7 @@ class Vector(object):
                 raise Exception('Cannot compute an angle with the zero vector')
             else:
                 raise e
+
 
 vec1 = Vector([7.887, 4.138])
 vec2 = Vector([-8.802, 6.776])
