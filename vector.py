5a65a282f679e541ab3f8a7f1b7832385e10ae08
Ahmed_Gharib <A.Gharib89@yahoo.com>
2020-07-05 12:52:40 +0200

    adding code for adding, substracting and multiply functions

--- vector.py
+++ vector.py
@@ -4,7 +4,7 @@ class Vector(object):
             if not coordinates:
                 raise ValueError
             self.coordinates = tuple(coordinates)
-            self.dimintion = len(coordinates)
+            self.dimension = len(coordinates)
 
         except ValueError:
             raise ValueError('The coordinates must be nonempty')
@@ -18,6 +18,22 @@ class Vector(object):
     def __eq__(self, v):
         return self.coordinates == v.coordinates
 
-vector = Vector([1, 2, 3])
+    def __add__(self, v):
+        if self.dimension != v.dimension:
+            raise ValueError('The 2 vectors must have the same dimensions')
+        else:
+            return Vector([self.coordinates[i] + v.coordinates[i] for i in range(len(self.coordinates))])
 
-print(vector)
+    def __sub__(self, v):
+        if self.dimension != v.dimension:
+            raise ValueError('The 2 vectors must have the same dimensions')
+        else:
+            return Vector([self.coordinates[i] - v.coordinates[i] for i in range(len(self.coordinates))])
+
+    def __mul__(self, n):
+        return Vector([self.coordinates[i] * n for i in range(len(self.coordinates))])
+
+
+vector = Vector([1.671, -1.012, -0.318])
+vector2 = Vector([-8.223, 0.878])
+print(vector * 7.41)
